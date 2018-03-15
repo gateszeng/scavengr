f99e1ef144b566d2fbe6f9e2ec7896142e9df07d
Gates Zeng <gatesazeng@gmail.com>
2018-03-15 11:06:38 -0700

    basic single player complete

---
+++ Scavengr/ScavengerEngine.swift
@@ -0,0 +1,13 @@
+//
+//  ScavengerEngine.swift
+//  Scavengr
+//
+//  Created by Gates Zeng on 3/15/18.
+//  Copyright © 2018 Gates Zeng. All rights reserved.
+//
+
+import UIKit
+
+class ScavengerEngine: NSObject {
+
+}
